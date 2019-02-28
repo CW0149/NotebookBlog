9c6c65cfe6ee2661b60d9b0fd88a4b3f75e7c2db
RyanHuang <ryan_0149@qq.com>
2019-02-28 11:59:42 +0800

    .

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Footer(props) {
+	return (
+		<div className="app-footer"><a href="http://www.miibeian.gov.cn" target="_blank" rel="noopener noreferrer">粤ICP备19004101号-1</a></div>
+	)
+}
+
+
+export default Footer
